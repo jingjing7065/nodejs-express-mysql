d7cd21ec32d1bed33e6c50313f9495274c531aa9
jingjing7065 <52123426+jingjing7065@users.noreply.github.com>
2025-11-19 19:14:52 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ require("./app/routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
